2635d5497beb8ebd5dd7f37d733fefe104a93ca6
Dmenefield <dmenefieldjr@gmail.com>
2021-09-02 16:19:40 -0700

    changed about img

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import contactPagePicture from "../../../static/assets/images/auth/profile-pic.png";
+import headshot from "../../../static/assets/images/auth/Headshot.png";
 export default function() {
   return (
       <div className="content-page-wrapper">
@@ -9,16 +9,18 @@ export default function() {
         fontFamily: 'inherit'
       }} >  Hello!  I am a full stack web developer born in Woodland hills, California with about 3 years of coding experience. 
 Certified in:  Javascript, ReactJS, Python 3, HTML5, CSS, JSON, Git, SQL Databases, SCSS/SASS, Flexbox, & CSS Grid. Growing up, videogames and computers made me very passionate about technology and what drew me to programming was the endless possibilities that a career in coding provides. Coding allows me to express my passion for storytelling by bringing my clients story to life through web design. Ultimately my goal is to create beautiful things that serve beautiful purposes.
-
+{/*"url(" + contactPagePicture + ") no-repeat",*/}
      </div>
         <div 
-        className="right-column"
+        className="right-column__about"
         style={{
-          background: "url(" + contactPagePicture + ") no-repeat",
+          background: "white",
           backgroundSize: "cover",
           backgroundPosition: "center"
         }}>
-
+          <div className="about-img">
+            <img src={headshot}/>
+          </div>
        
       </div>
       </div>
